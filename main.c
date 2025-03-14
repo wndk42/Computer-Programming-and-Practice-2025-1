d204ecf0d12ea7a0ef09dba046e79b3a5df135a7
wndk42 <tealblue568@gmail.com>
2025-03-14 18:27:03 +0900

    .

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 int value(int);
 
 int main() {
-	return value(1); /* 1을 반환한다. */
+	return value(100); /* 1을 반환한다. */
 }
 
 /* To Do: value.c 소스 파일을 만들고, 새로 만든 파일에서 value 함수를 정의해보세요 */
---
+++ value.c
@@ -0,0 +1,5 @@
+int value(int);
+
+int value(int x) {
+	return x;
+}
