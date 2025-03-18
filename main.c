81ec27552f5ec5e4abc9abaca3d4c22cbcb8e176
chaebkimm <chae.b.kim@gmail.com>
2025-03-18 20:04:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,4 +7,9 @@
  * 포인터는 해당 타입 변수의 주소를 저장할 수 있는 타입입니다.  
  */
 
-int i; /* To Do: i의 타입을 int 포인터 타입으로 바꿔보세요 */
+int i = 0xaabbccdd;
+int pi; /* To Do: pi의 타입을 int 포인터 타입으로 바꾸고 초기값을 i의 주소로 설정해보세요 */
+
+int main() {
+  return 0;
+}
