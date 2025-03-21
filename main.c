95811d7e6179946565281f22ba2f771ae2e5fc48
wndk42 <tealblue568@gmail.com>
2025-03-21 17:36:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,10 @@ void print_char(char c);
 
 int main() {
 	/* To Do: print_char 함수를 여러번 호출해서 화면에 linux라고 표시해보세요 */
-
-	
+	print_char('l');
+	print_char('i');
+	print_char('n');
+	print_char('u');
+	print_char('x');
 	return 0;
 }
